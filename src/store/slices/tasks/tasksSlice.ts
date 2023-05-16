cffd36081eb5bd7a819f972719d53b09e27b6a09
Dixon Gutiérrez <96222189+dgutierrez21@users.noreply.github.com>
2023-05-16 10:03:54 -0600

    add data to the initialState

--- src/store/slices/tasks/tasksSlice.ts
+++ src/store/slices/tasks/tasksSlice.ts
@@ -1,12 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit'
-import type {PayloadAction} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: any[] = [];
+interface TaskState {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+const initialState: TaskState[] = [
+  {
+    id: "1",
+    title: "Task 1",
+    description: "Task 1 description",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    description: "Task 2 description",
+    completed: false,
+  },
+];
 
 export const tasksSlice = createSlice({
-    name: "tasks",
-    initialState,
-    reducers: {
-        
-    }
-})
+  name: "tasks",
+  initialState,
+  reducers: {},
+});
